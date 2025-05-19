a0170ac73b4e693cd4dabecd84c612ebfc10c9b5
dogusblkbs <80848971+dogusblkbs@users.noreply.github.com>
2025-05-19 20:49:49 +0300

    Vtys.sql

---
+++ Vtys.sql
@@ -0,0 +1,566 @@
+Create Database Powerbank
+
+Create table Kullanici
+(
+id int primary key,
+kullaniciAdi varchar(50)not null ,
+kullaniciSoyad varchar(50) not null,
+KullaniciYas int,
+KullaniciCinsiyet varchar(50)
+)
+
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (1, 'Ahmet', 'Yılmaz', 22, 'Erkek');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (2, 'Elif', 'Demir', 35, 'Kadın');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (3, 'Mert', 'Şahin', 28, 'Erkek');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (4, 'Zeynep', 'Koç', 19, 'Kadın');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (5, 'Burak', 'Aydın', 41, 'Erkek');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (6, 'Ayşe', 'Çelik', 33, 'Kadın');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (7, 'Emre', 'Arslan', 26, 'Erkek');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (8, 'Fatma', 'Kaya', 24, 'Kadın');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (9, 'Can', 'Yıldız', 37, 'Erkek');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (10, 'Hatice', 'Bulut', 18, 'Kadın');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (11, 'Kerem', 'Güneş', 44, 'Erkek');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (12, 'Derya', 'Bozkurt', 31, 'Kadın');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (13, 'Tuna', 'Er', 21, 'Erkek');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (14, 'Esra', 'Sezer', 29, 'Kadın');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (15, 'Onur', 'Aksoy', 46, 'Erkek');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (16, 'Selin', 'Kurt', 23, 'Kadın');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (17, 'Eren', 'Öztürk', 20, 'Erkek');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (18, 'Melis', 'Polat', 27, 'Kadın');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (19, 'Baran', 'Taş', 36, 'Erkek');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (20, 'Yasemin', 'Sarı', 25, 'Kadın');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (21, 'Umut', 'Acar', 38, 'Erkek');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (22, 'Cansu', 'Kılıç', 34, 'Kadın');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (23, 'Volkan', 'Topal', 30, 'Erkek');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (24, 'Nazlı', 'Kaya', 40, 'Kadın');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (25, 'Okan', 'Işık', 32, 'Erkek');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (26, 'Büşra', 'Uçar', 39, 'Kadın');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (27, 'Hakan', 'Kara', 17, 'Erkek');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (28, 'İrem', 'Aslan', 16, 'Kadın');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (29, 'Furkan', 'Ersoy', 45, 'Erkek');
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (30, 'Damla', 'Sağlam', 19, 'Kadın');
+
+
+
+
+CREATE TABLE Powerbank (
+    PowerbankID INT PRIMARY KEY,
+    UretimYili INT CHECK (UretimYili IN (2024, 2025)),
+    SonKullananID INT,
+    SonKiralamaTarihi DATE,
+    FOREIGN KEY (SonKullananID) REFERENCES Kullanici(id)
+	)
+
+INSERT INTO Powerbank (PowerbankID, UretimYili, SonKullananID, SonKiralamaTarihi) VALUES
+(1, 2024, 5, '2025-05-02'),
+(2, 2025, 17, '2025-05-11'),
+(3, 2024, 9, '2025-05-23'),
+(4, 2025, 1, '2025-05-30'),
+(5, 2025, 12, '2025-05-18'),
+(6, 2024, 8, '2025-05-08'),
+(7, 2025, 21, '2025-05-26'),
+(8, 2025, 4, '2025-05-15'),
+(9, 2024, 16, '2025-05-07'),
+(10, 2025, 29, '2025-05-28'),
+(11, 2024, 3, '2025-05-04'),
+(12, 2024, 11, '2025-05-12'),
+(13, 2025, 18, '2025-05-22'),
+(14, 2024, 7, '2025-05-03'),
+(15, 2025, 26, '2025-05-27'),
+(16, 2025, 14, '2025-05-01'),
+(17, 2024, 6, '2025-05-05'),
+(18, 2024, 24, '2025-05-09'),
+(19, 2025, 30, '2025-05-17'),
+(20, 2025, 10, '2025-05-13'),
+(21, 2024, 15, '2025-05-06'),
+(22, 2025, 22, '2025-05-29'),
+(23, 2025, 2, '2025-05-21'),
+(24, 2024, 13, '2025-05-19'),
+(25, 2024, 25, '2025-05-20'),
+(26, 2025, 23, '2025-05-16'),
+(27, 2025, 19, '2025-05-10'),
+(28, 2024, 28, '2025-05-24'),
+(29, 2025, 27, '2025-05-25'),
+(30, 2024, 20, '2025-05-14'),
+(31, 2025, 5, '2025-05-11'),
+(32, 2025, 17, '2025-05-12'),
+(33, 2024, 9, '2025-05-23'),
+(34, 2025, 1, '2025-05-04'),
+(35, 2025, 12, '2025-05-18'),
+(36, 2024, 8, '2025-05-08'),
+(37, 2025, 21, '2025-05-26'),
+(38, 2025, 4, '2025-05-15'),
+(39, 2024, 16, '2025-05-07'),
+(40, 2025, 29, '2025-05-28'),
+(41, 2024, 3, '2025-05-04'),
+(42, 2024, 11, '2025-05-12'),
+(43, 2025, 18, '2025-05-22'),
+(44, 2024, 7, '2025-05-03'),
+(45, 2025, 26, '2025-05-27'),
+(46, 2025, 14, '2025-05-01'),
+(47, 2024, 6, '2025-05-05'),
+(48, 2024, 24, '2025-05-09'),
+(49, 2025, 30, '2025-05-17'),
+(50, 2025, 10, '2025-05-13');
+
+
+CREATE TABLE Kiralama (
+    KiralamaID INTEGER PRIMARY KEY,
+    KullaniciID INTEGER,
+    PowerbankID INTEGER,
+    KiralamaTarihi DATE,
+    KullanimSuresiDakika INTEGER,
+    DakikalikUcret DECIMAL(5,2) DEFAULT 1.00,
+    ToplamUcret DECIMAL(6,2),
+    FOREIGN KEY (KullaniciID) REFERENCES Kullanici(id),
+    FOREIGN KEY (PowerbankID) REFERENCES Powerbank(PowerbankID)
+);
+
+INSERT INTO Kiralama (KiralamaId, KullaniciId, PowerbankId, KiralamaTarihi, KullanimSuresiDakika, DakikalikUcret, ToplamUcret)
+VALUES
+(1, 12, 5, '2025-05-22', 95, 1, 95),
+(2, 3, 37, '2025-05-18', 140, 1, 140),
+(3, 28, 19, '2025-05-09', 123, 1, 123),
+(4, 7, 1, '2025-05-03', 178, 1, 178),
+(5, 25, 7, '2025-05-29', 51, 1, 51),
+(6, 11, 12, '2025-05-16', 177, 1, 177),
+(7, 15, 27, '2025-05-11', 133, 1, 133),
+(8, 22, 9, '2025-05-02', 69, 1, 69),
+(9, 5, 43, '2025-05-27', 51, 1, 51),
+(10, 30, 3, '2025-05-21', 178, 1, 178),
+(11, 1, 48, '2025-05-20', 161, 1, 161),
+(12, 16, 38, '2025-05-25', 169, 1, 169),
+(13, 14, 50, '2025-05-15', 111, 1, 111),
+(14, 9, 4, '2025-05-05', 157, 1, 157),
+(15, 24, 6, '2025-05-06', 120, 1, 120),
+(16, 19, 21, '2025-05-26', 166, 1, 166),
+(17, 10, 15, '2025-05-07', 30, 1, 30),
+(18, 2, 29, '2025-05-04', 97, 1, 97),
+(19, 20, 18, '2025-05-19', 86, 1, 86),
+(20, 27, 10, '2025-05-08', 124, 1, 124),
+(21, 8, 41, '2025-05-10', 172, 1, 172),
+(22, 6, 14, '2025-05-23', 70, 1, 70),
+(23, 17, 30, '2025-05-14', 60, 1, 60),
+(24, 4, 44, '2025-05-01', 138, 1, 138),
+(25, 21, 36, '2025-05-12', 85, 1, 85),
+(26, 13, 32, '2025-05-13', 114, 1, 114),
+(27, 23, 25, '2025-05-17', 134, 1, 134),
+(28, 18, 16, '2025-05-28', 108, 1, 108),
+(29, 29, 11, '2025-05-24', 55, 1, 55),
+(30, 26, 22, '2025-05-30', 84, 1, 84),
+(31, 7, 9, '2025-05-16', 98, 1, 98),
+(32, 11, 2, '2025-05-20', 59, 1, 59),
+(33, 30, 45, '2025-05-27', 176, 1, 176),
+(34, 19, 35, '2025-05-09', 132, 1, 132),
+(35, 24, 48, '2025-05-11', 176, 1, 176),
+(36, 15, 7, '2025-05-22', 141, 1, 141),
+(37, 21, 19, '2025-05-29', 72, 1, 72),
+(38, 13, 6, '2025-05-03', 101, 1, 101),
+(39, 1, 28, '2025-05-14', 167, 1, 167),
+(40, 9, 34, '2025-05-26', 80, 1, 80),
+(41, 16, 31, '2025-05-15', 48, 1, 48),
+(42, 3, 12, '2025-05-07', 176, 1, 176),
+(43, 26, 40, '2025-05-01', 63, 1, 63),
+(44, 17, 14, '2025-05-10', 154, 1, 154),
+(45, 28, 5, '2025-05-18', 132, 1, 132),
+(46, 4, 24, '2025-05-23', 135, 1, 135),
+(47, 30, 27, '2025-05-20', 57, 1, 57),
+(48, 12, 18, '2025-05-06', 138, 1, 138),
+(49, 8, 33, '2025-05-28', 178, 1, 178),
+(50, 2, 44, '2025-05-25', 103, 1, 103),
+(51, 29, 30, '2025-05-19', 112, 1, 112),
+(52, 22, 8, '2025-05-21', 33, 1, 33),
+(53, 18, 4, '2025-05-13', 177, 1, 177),
+(54, 7, 41, '2025-05-12', 68, 1, 68),
+(55, 11, 39, '2025-05-30', 151, 1, 151),
+(56, 20, 10, '2025-05-17', 122, 1, 122),
+(57, 9, 37, '2025-05-24', 173, 1, 173),
+(58, 25, 22, '2025-05-26', 97, 1, 97),
+(59, 13, 16, '2025-05-28', 154, 1, 154),
+(60, 5, 3, '2025-05-15', 165, 1, 165),
+(61, 14, 20, '2025-05-05', 98, 1, 98),
+(62, 6, 21, '2025-05-06', 54, 1, 54),
+(63, 10, 50, '2025-05-27', 97, 1, 97),
+(64, 3, 13, '2025-05-02', 136, 1, 136),
+(65, 27, 35, '2025-05-29', 134, 1, 134),
+(66, 1, 7, '2025-05-16', 48, 1, 48),
+(67, 20, 26, '2025-05-20', 170, 1, 170),
+(68, 16, 9, '2025-05-22', 161, 1, 161),
+(69, 24, 19, '2025-05-07', 150, 1, 150),
+(70, 9, 15, '2025-05-18', 116, 1, 116),
+(71, 18, 32, '2025-05-11', 86, 1, 86),
+(72, 11, 5, '2025-05-10', 142, 1, 142),
+(73, 28, 4, '2025-05-03', 70, 1, 70),
+(74, 13, 27, '2025-05-01', 150, 1, 150),
+(75, 7, 14, '2025-05-09', 169, 1, 169),
+(76, 30, 24, '2025-05-28', 171, 1, 171),
+(77, 22, 44, '2025-05-23', 67, 1, 67),
+(78, 4, 29, '2025-05-25', 144, 1, 144),
+(79, 15, 43, '2025-05-14', 160, 1, 160),
+(80, 19, 31, '2025-05-06', 99, 1, 99),
+(81, 5, 40, '2025-05-17', 124, 1, 124),
+(82, 2, 28, '2025-05-26', 138, 1, 138),
+(83, 6, 39, '2025-05-04', 111, 1, 111),
+(84, 9, 13, '2025-05-08', 144, 1, 144),
+(85, 1, 20, '2025-05-27', 51, 1, 51),
+(86, 25, 33, '2025-05-21', 111, 1, 111),
+(87, 27, 6, '2025-05-19', 92, 1, 92),
+(88, 23, 21, '2025-05-24', 138, 1, 138),
+(89, 10, 50, '2025-05-15', 75, 1, 75),
+(90, 14, 8, '2025-05-29', 171, 1, 171),
+(91, 17, 12, '2025-05-18', 33, 1, 33),
+(92, 3, 11, '2025-05-07', 123, 1, 123),
+(93, 8, 16, '2025-05-12', 59, 1, 59),
+(94, 21, 34, '2025-05-13', 140, 1, 140),
+(95, 29, 19, '2025-05-01', 173, 1, 173),
+(96, 18, 7, '2025-05-22', 81, 1, 81),
+(97, 5, 5, '2025-05-10', 72, 1, 72),
+(98, 16, 3, '2025-05-15', 58, 1, 58),
+(99, 26, 25, '2025-05-20', 108, 1, 108),
+(100, 12, 2, '2025-05-23', 155, 1, 155);
+
+CREATE TABLE Lokasyon (
+    Id INT PRIMARY KEY,
+    Sehir VARCHAR(50) NOT NULL
+);
+
+INSERT INTO Lokasyon (Id, Sehir) VALUES
+(1, 'İstanbul'),
+(2, 'Ankara'),
+(3, 'İzmir');
+
+
+
+CREATE TABLE KiralamaLokasyon (
+    LokasyonId INT,                          
+    KiralamaId INT primary key,                           
+    KullaniciId INT,                         
+    FOREIGN KEY (LokasyonId) REFERENCES Lokasyon(Id),
+    FOREIGN KEY (KiralamaId) REFERENCES Kiralama(KiralamaID),
+    FOREIGN KEY (KullaniciId) REFERENCES Kullanici(id)
+)
+
+INSERT INTO KiralamaLokasyon(LokasyonId, KiralamaId, KullaniciId) VALUES
+(1, 1, 5),
+(3, 2, 12),
+(2, 3, 27),
+(1, 4, 7),
+(2, 5, 9),
+(3, 6, 2),
+(1, 7, 14),
+(2, 8, 1),
+(3, 9, 6),
+(1, 10, 17),
+(3, 11, 23),
+(2, 12, 15),
+(1, 13, 11),
+(2, 14, 28),
+(3, 15, 4),
+(1, 16, 19),
+(2, 17, 13),
+(3, 18, 3),
+(1, 19, 26),
+(2, 20, 8),
+(3, 21, 30),
+(1, 22, 21),
+(2, 23, 20),
+(3, 24, 22),
+(1, 25, 10),
+(2, 26, 29),
+(3, 27, 24),
+(1, 28, 16),
+(2, 29, 25),
+(3, 30, 18),
+(1, 31, 7),
+(3, 32, 2),
+(2, 33, 6),
+(1, 34, 12),
+(3, 35, 1),
+(2, 36, 9),
+(1, 37, 14),
+(3, 38, 5),
+(2, 39, 13),
+(1, 40, 17),
+(2, 41, 28),
+(3, 42, 4),
+(1, 43, 19),
+(2, 44, 11),
+(3, 45, 3),
+(1, 46, 23),
+(2, 47, 27),
+(3, 48, 8),
+(1, 49, 22),
+(2, 50, 10),
+(3, 51, 30),
+(1, 52, 24),
+(2, 53, 16),
+(3, 54, 15),
+(1, 55, 20),
+(2, 56, 29),
+(3, 57, 26),
+(1, 58, 25),
+(2, 59, 21),
+(3, 60, 18),
+(1, 61, 13),
+(2, 62, 7),
+(3, 63, 6),
+(1, 64, 12),
+(2, 65, 1),
+(3, 66, 9),
+(1, 67, 5),
+(2, 68, 28),
+(3, 69, 4),
+(1, 70, 17),
+(2, 71, 14),
+(3, 72, 2),
+(1, 73, 23),
+(2, 74, 3),
+(3, 75, 11),
+(1, 76, 19),
+(2, 77, 27),
+(3, 78, 8),
+(1, 79, 10),
+(2, 80, 22),
+(3, 81, 30),
+(1, 82, 24),
+(2, 83, 16),
+(3, 84, 15),
+(1, 85, 20),
+(2, 86, 29),
+(3, 87, 26),
+(1, 88, 25),
+(2, 89, 21),
+(3, 90, 18),
+(1, 91, 13),
+(2, 92, 7),
+(3, 93, 6),
+(1, 94, 12),
+(2, 95, 1),
+(3, 96, 9),
+(1, 97, 5),
+(2, 98, 28),
+(3, 99, 4),
+(1, 100, 17);
+
+
+
+UPDATE Kullanici      -- Id'si 23 olan kullanıcının ismini Doğuş olarak güncelledik
+SET KullaniciAdi = 'Doğuş'
+WHERE id = 23;
+
+
+INSERT INTO Lokasyon (Id, Sehir) VALUES
+(4 , 'Bursa')
+
+DELETE FROM Powerbank  -- Id'si 98 olan Powerbank arızalandığı için bu powerbanki veri tabanından çıkarıyorum.
+WHERE PowerbankID = 98;
+
+
+-- Sorgular ,DDL kodları , DML kodları, View ve Stored Procedure
+--DDL
+CREATE TABLE Powerbank (
+PowerbankID INT PRIMARY KEY,
+UretimYili INT CHECK (UretimYili IN (2024, 2025)),
+SonKullananID INT,
+SonKiralamaTarihi DATE,
+FOREIGN KEY (SonKullananID) REFERENCES Kullanici(id))
+
+Create table Kullanici
+(
+id int primary key,
+kullaniciAdi varchar(50)not null ,
+kullaniciSoyad varchar(50) not null,
+KullaniciYas int,
+KullaniciCinsiyet text )
+
+
+
+-- DML
+
+INSERT INTO Kullanici (id, KullaniciAdi, KullaniciSoyad, KullaniciYas, KullaniciCinsiyet) VALUES (1, 'Ahmet', 'Yılmaz', 22, 'Erkek');
+
+INSERT INTO Powerbank (PowerbankID, UretimYili, SonKullananID, SonKiralamaTarihi) VALUES (1, 2024, 5, '2025-05-02'),
+
+UPDATE Kullanici  SET KullaniciAdi = 'Doğuş' WHERE id = 23; (Id'si 23 olan kullanıcının ismini Doğuş olarak güncelledi)
+
+DELETE FROM Powerbank  WHERE PowerbankID = 98 ; ( Id'si 98 olan Powerbank arızalandığı için bu powerbanki veri tabanından çıkarıyorum.
+
+INSERT INTO Lokasyon (Id, Sehir) VALUES (4 , 'Bursa') (Şirket Artık Bursa şehrinde de hizmet verdiği için Bursa şehrini lokasyon tablomuza ekledik.)
+
+
+-- SORGULAR
+
+Select*From Kullanici order By kullaniciyas desc
+
+
+Select*From Kullanici where KullaniciCinsiyet = 'Kadın'
+
+Select KullaniciAdi From Kullanici
+
+SELECT AVG(KullaniciYas) AS OrtalamaYas FROM Kullanici;
+
+SELECT AVG(KullaniciYas) AS KadınOrtalamaYas FROM Kullanici where KullaniciCinsiyet = 'Kadın';
+
+-- Yaş Gruplama
+SELECT 
+  CASE 
+    WHEN KullaniciYas BETWEEN 15 AND 20 THEN '15-20'
+    WHEN KullaniciYas > 20 AND KullaniciYas <= 25 THEN '21-25'
+    WHEN KullaniciYas > 25 AND KullaniciYas <= 30 THEN '26-30'
+    WHEN KullaniciYas > 30 AND KullaniciYas <= 35 THEN '31-35'
+    ELSE '35+'
+  END AS YasAraligi,
+  COUNT(*) AS KullaniciSayisi
+FROM Kullanici
+GROUP BY 
+  CASE 
+    WHEN KullaniciYas BETWEEN 15 AND 20 THEN '15-20'
+    WHEN KullaniciYas > 20 AND KullaniciYas <= 25 THEN '21-25'
+    WHEN KullaniciYas > 25 AND KullaniciYas <= 30 THEN '26-30'
+    WHEN KullaniciYas > 30 AND KullaniciYas <= 35 THEN '31-35'
+    ELSE '35+'
+  END
+ORDER BY 
+  MIN(KullaniciYas);
+
+
+--Her powerbank ile kaç adet kiralama yapıldığını ve powerbank başına kazanılan ücreti görelim.
+SELECT 
+    p.PowerbankID,
+    COUNT(k.KiralamaID) AS KiralamaSayisi,
+    ISNULL(SUM(k.ToplamUcret), 0) AS ToplamKazanc
+FROM 
+    Powerbank p
+LEFT JOIN 
+    Kiralama k ON p.PowerbankID = k.PowerbankID
+GROUP BY 
+    p.PowerbankID
+ORDER BY 
+    p.PowerbankID;
+
+-- Yapılan kiralamaları dakikasına göre gruplayalım ve yanlarına toplam ücretlerini ekleyelim.
+SELECT 
+    CASE 
+        WHEN KullanimSuresiDakika BETWEEN 0 AND 30 THEN '0-30 Dakika'
+        WHEN KullanimSuresiDakika BETWEEN 31 AND 60 THEN '31-60 Dakika'
+        WHEN KullanimSuresiDakika BETWEEN 61 AND 90 THEN '61-90 Dakika'
+        WHEN KullanimSuresiDakika BETWEEN 91 AND 120 THEN '91-120 Dakika'
+        WHEN KullanimSuresiDakika BETWEEN 121 AND 150 THEN '121-150 Dakika'
+        WHEN KullanimSuresiDakika BETWEEN 151 AND 180 THEN '151-180 Dakika'
+        ELSE '180+ Dakika'
+    END AS SureAraligi,
+    COUNT(*) AS KiralamaSayisi,
+    SUM(ToplamUcret) AS ToplamKazanc
+FROM 
+    Kiralama
+GROUP BY 
+    CASE 
+        WHEN KullanimSuresiDakika BETWEEN 0 AND 30 THEN '0-30 Dakika'
+        WHEN KullanimSuresiDakika BETWEEN 31 AND 60 THEN '31-60 Dakika'
+        WHEN KullanimSuresiDakika BETWEEN 61 AND 90 THEN '61-90 Dakika'
+        WHEN KullanimSuresiDakika BETWEEN 91 AND 120 THEN '91-120 Dakika'
+        WHEN KullanimSuresiDakika BETWEEN 121 AND 150 THEN '121-150 Dakika'
+        WHEN KullanimSuresiDakika BETWEEN 151 AND 180 THEN '151-180 Dakika'
+        ELSE '180+ Dakika'
+    END
+ORDER BY
+    MIN(KullanimSuresiDakika);
+
+
+
+
+-- Kullanıcıları yaptıkları kiralama sayısına göre sıraladık. Ve toplam kazandığımız ücrete göre sıraladık.
+
+SELECT 
+    ku.KullaniciAdi, 
+    ku.KullaniciSoyad, 
+    ku.KullaniciYas,
+    COUNT(k.KiralamaID) AS KiralamaSayisi,
+    ISNULL(SUM(k.ToplamUcret), 0) AS ToplamUcret
+FROM 
+    Kullanici ku
+LEFT JOIN 
+    Kiralama k ON ku.id = k.KullaniciID
+GROUP BY 
+    ku.KullaniciAdi, ku.KullaniciSoyad, ku.KullaniciYas
+Order By ToplamUcret Desc;
+
+-- Her Lokasyon için daha önce belirlediğimiz dakika aralıklarına göre grupladık ve ücretlerini yanına yazdık ve en yüksek ücrete göre sıraladık.
+
+SELECT 
+    l.Sehir,
+    CASE
+        WHEN k.KullanimSuresiDakika BETWEEN 0 AND 30 THEN '0-30 Dakika'
+        WHEN k.KullanimSuresiDakika BETWEEN 31 AND 60 THEN '30-60 Dakika'
+        WHEN k.KullanimSuresiDakika BETWEEN 61 AND 90 THEN '60-90 Dakika'
+        WHEN k.KullanimSuresiDakika BETWEEN 91 AND 120 THEN '90-120 Dakika'
+        WHEN k.KullanimSuresiDakika BETWEEN 121 AND 150 THEN '120-150 Dakika'
+        WHEN k.KullanimSuresiDakika BETWEEN 151 AND 180 THEN '150-180 Dakika'
+        ELSE '180+ Dakika'
+    END AS SureAraligi,
+    COUNT(k.KiralamaID) AS KiralamaSayisi,
+    SUM(k.ToplamUcret) AS ToplamUcret
+FROM 
+    Kiralama k
+JOIN 
+    KiralamaLokasyon kl ON k.KiralamaID = kl.KiralamaID
+JOIN 
+    Lokasyon l ON kl.LokasyonID = l.Id
+GROUP BY 
+    l.Sehir,
+    CASE
+        WHEN k.KullanimSuresiDakika BETWEEN 0 AND 30 THEN '0-30 Dakika'
+        WHEN k.KullanimSuresiDakika BETWEEN 31 AND 60 THEN '30-60 Dakika'
+        WHEN k.KullanimSuresiDakika BETWEEN 61 AND 90 THEN '60-90 Dakika'
+        WHEN k.KullanimSuresiDakika BETWEEN 91 AND 120 THEN '90-120 Dakika'
+        WHEN k.KullanimSuresiDakika BETWEEN 121 AND 150 THEN '120-150 Dakika'
+        WHEN k.KullanimSuresiDakika BETWEEN 151 AND 180 THEN '150-180 Dakika'
+        ELSE '180+ Dakika'
+    END
+ORDER BY 
+    ToplamUcret Desc;
+
+
+
+-- VİEW :Powerbank kullanım özetini gösteren bir görünüm oluşturalım.
+
+
+CREATE VIEW PowerbankKullanimOzet AS
+SELECT 
+    p.PowerbankID,
+    COUNT(k.KiralamaID) AS ToplamKiralamaSayisi,
+    SUM(k.ToplamUcret) AS ToplamKazanc
+FROM 
+    Powerbank p
+LEFT JOIN 
+    Kiralama k ON p.PowerbankID = k.PowerbankID
+GROUP BY 
+    p.PowerbankID;
+
+
+--Kullanım : 
+SELECT * FROM PowerbankKullanimOzet;
+
+
+-- Stored Procedure: (Saklı Yordam), tekrar tekrar çalıştırılmak üzere tanımlanmış SQL komutlarının bir blok halinde veritabanında saklanmasıdır.
+--Her lokasyon için kiralama bilgileri getiren bir sorgu hazırladım
+CREATE PROCEDURE LokasyonaGoreKiralama
+ @LokasyonID INT
+AS
+BEGIN
+SELECT
+	k.KiralamaID,
+	k.KullaniciID,
+	k.PowerbankID,
+	k.KiralamaTarihi,
+	k.KullanimSuresiDakika,
+	k.ToplamUcret
+FROM 
+Kiralama k
+INNER JOIN 
+KiralamaLokasyon kl ON k.KiralamaID = kl.KiralamaID
+WHERE 
+kl.LokasyonID = @LokasyonID;
+END;
+Lokasyon ID 3 olanları getirelim
+EXEC LokasyonaGoreKiralama @LokasyonID = 3;
